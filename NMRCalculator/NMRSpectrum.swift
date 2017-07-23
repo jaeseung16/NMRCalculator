c844d978389cb74a007601745ae7aec4c0d67381
Jae-Seung Lee <jaeseung@gmail.com>
2017-07-23 14:11:14 -0400

    Updated NMRSpectrum.swift

--- NMRCalculator/NMRSpectrum.swift
+++ NMRCalculator/NMRSpectrum.swift
@@ -8,12 +8,12 @@
 
 import Foundation
 
-class NMRSpectrum {
-    var size: UInt? // number of data points
-    var width: Double? // spectral width in kHz
-    var resolution: Double? // spectral resolution in Hz
-    var real: [Double] // real part
-    var imag: [Double] // imaginary part
+struct NMRSpectrum {
+    var size: UInt = 0 // number of data points
+    var width: Double = 0.0 // spectral width in kHz
+    var resolution: Double = 0.0 // spectral resolution in Hz
+    //    var real: [Double] // real part
+    //    var imag: [Double] // imaginary part
     
     enum parameters: String {
         case size
@@ -22,59 +22,57 @@ class NMRSpectrum {
     }
     
     init() {
-        size = UInt()
-        width = Double()
-        resolution = Double()
-        real = [Double]()
-        imag = [Double]()
-    }
-    
-    func set_parameter(_ name: String, to_value: Double) -> Bool {
-        if let to_set = parameters(rawValue: name) {
-            switch to_set {
-            case .size:
-                if to_value <= Double( UInt.max ) && to_value > Double( UInt.min ) {
-                    size = UInt(to_value)
-                    return true
-                }
-            case .width:
-                if to_value > 0 {
-                    width = to_value
-                    return true
-                }
-            case .resolution:
-                if to_value > 0 {
-                    resolution = to_value
-                    return true
-                }
-            }
-        }
-        
-        return false
         
     }
     
-    func update(_ name: String) -> Bool {
-        if let to_update = parameters(rawValue: name) {
-            switch to_update {
-            case .size:
-                if width != nil && resolution != nil {
-                    size = UInt( 1000.0 * width! / resolution! )
-                        return true
-                }
-            case .width:
-                if size != nil && resolution != nil {
-                    width = Double(size!) * resolution! / 1000.0
-                    return true
-                }
-            case .resolution:
-                if size != nil && width != nil {
-                    resolution = 1000.0 * width! / Double(size!)
-                    return true
-                }
-            }
+    mutating func setSpectrum(parameter name: String, to value: Double) -> Bool {
+        
+        guard let parameter = parameters(rawValue: name) else { return false }
+        
+        switch parameter {
+        case .size:
+            guard ( value <= Double( UInt.max ) ) && ( value > Double( UInt.min ) ) else { return false }
+            self.size = UInt(value)
+            
+        case .width:
+            guard value > 0 else { return false }
+            self.width = value
+            
+        case .resolution:
+            guard value > 0 else { return false }
+            self.resolution = value
         }
         
-        return false
+        return true
+        
+    }
+    
+    mutating func updateParameter(name: String) -> Bool {
+        guard let parameter = parameters(rawValue: name) else { return false }
+        
+        switch parameter {
+        case .size:
+            guard self.resolution > 0 else { return false }
+            self.size = UInt( 1000.0 * self.width / self.resolution )
+            
+        case .width:
+            self.width = Double(self.size) * self.resolution / 1000.0
+            
+        case .resolution:
+            guard self.size > 0 else { return false }
+            self.resolution = 1000.0 * self.width / Double(self.size)
+        }
+        
+        return true
+    }
+    
+    func describe() -> String {
+        let string1 = "Spectrum size = \(self.size)"
+        
+        let string2 = "Spectral width = \(self.width) kHz"
+        
+        let string3 = "Resolution = \(self.resolution) Hz"
+        
+        return string1 + "\n" + string2 + "\n" + string3
     }
 }
