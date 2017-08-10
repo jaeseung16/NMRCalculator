d353d8d2c8853de870b5a9b1bb6e47902d99951f
Jae-Seung Lee <jaeseung@gmail.com>
2017-08-10 19:36:38 -0400

    Updated ChemCalc.swift

--- NMRCalculator/ChemCalc.swift
+++ NMRCalculator/ChemCalc.swift
@@ -9,17 +9,19 @@
 import Foundation
 
 class ChemCalc {
-    let mw_h2o: Double = 18.0
-    var molecularweight: Double?
-    var molL: Double? // mol/L
-    var massfraction: Double? // mass fraction
-    var mol: Double? // solute amount in mol
-    var weight: Double? // solute amount in gram
-    var amount_h2o: Double? // solvent amount in gram or mL
-    var chemicalName: String?
+    let mwH2O: Double = 18.0
+    var molecularWeight: Double = 100.0
+    var molConcentration: Double // mol/L
+    var wtConcentration: Double // mass fraction
+    var molSolute: Double // solute amount in mol
+    var gramSolute: Double = 1.0 // solute amount in gram
+    var amountSolvent: Double = 100.0 // solvent amount in gram or mL
+    var chemicalName = "Chemical Name (optional)"
     
     init() {
-
+        molSolute = gramSolute / molecularWeight
+        molConcentration = molSolute / (amountSolvent / 1000.0)
+        wtConcentration = gramSolute / (gramSolute + amountSolvent)
     }
     
     // MARK: Method to set parameter
@@ -27,142 +29,93 @@ class ChemCalc {
     enum chemCategory: String {
         case chemical
         case molecularWeight
-        case amountSolute
+        case molConcentration
+        case wtConcentration
+        case molSolute
+        case gramSolute
         case amountSolvent
     }
     
-    func setParameter(_ name: String, in category: String, to value: String) -> Bool {
-        guard let category = chemCategory(rawValue: category) else { return false }
+    func setParameter(_ name: String, to value: String) -> Bool {
+        guard let category = chemCategory(rawValue: name) else { return false }
         
         switch category {
         case .chemical:
             self.chemicalName = value
-        default:
-            break
-        }
-        return false
-        
-    }
-    
-    // MARK: set the name of a chemical
-    
-    func set_chemicalname(_ name: String) {
-        chemicalName = name
-    }
-    
-    // MARK: set the molecular weight of a chemical
-    
-    func set_molecularweight(_ mw: Double) -> Bool {
-        if mw > 0 {
-            molecularweight = mw
-            return true
-        }
-        
-        return false
-    }
-    
-    // MARK: set the amount of solute or solvent
-    
-    enum amount: String {
-        case solute
-        case solvent
-    }
-    
-    func set_amount(_ of: String, gram: Double) -> Bool {
-        if let to_set = amount(rawValue: of) {
+        case .molecularWeight:
+            guard let mw = Double(value) else { return false }
+            if mw > 0 {
+                self.molecularWeight = mw
+            } else {
+                return false
+            }
+        case .molConcentration:
+            guard let mol = Double(value) else { return false }
+            if mol > 0 {
+                self.molConcentration = mol
+            } else {
+                return false
+            }
+        case .wtConcentration:
+            guard let wt = Double(value) else { return false }
+            if wt > 0 {
+                self.wtConcentration = wt
+            } else {
+                return false
+            }
+        case .molSolute:
+            guard let mol = Double(value) else { return false }
+            if mol > 0 {
+                self.molSolute = mol
+            } else {
+                return false
+            }
+        case .gramSolute:
+            guard let gram = Double(value) else { return false }
             if gram > 0 {
-                switch to_set {
-                case .solute:
-                    weight = gram
-                case .solvent:
-                    amount_h2o = gram
-                }
-                return true
+                self.gramSolute = gram
+            } else {
+                return false
             }
+        case .amountSolvent:
+            guard let solvent = Double(value) else { return false }
+            if solvent > 0 {
+                self.amountSolvent = solvent
+            } else {
+                return false
+            }
+        default:
+            return false
+        }
+        return true
+        
+    }
+    
+    func updateParameter(_ name: String, flag: Bool = true) -> Bool {
+        guard let category = chemCategory(rawValue: name) else { return false }
+        
+        switch category {
+        case .molSolute:
+            if flag {
+                molSolute = gramSolute / molecularWeight
+            } else {
+                molSolute = molConcentration * (amountSolvent / 1000.0)
+            }
+        case .gramSolute:
+            if flag {
+                gramSolute = molSolute * molecularWeight
+            } else {
+                gramSolute = amountSolvent * wtConcentration / ( 1 - wtConcentration )
+            }
+        case .molConcentration:
+            molConcentration = molSolute / (amountSolvent / 1000.0)
+        case .wtConcentration:
+            wtConcentration = gramSolute / (gramSolute + amountSolvent)
+        default:
+            return false
         }
         
-        return false
+        return true
     }
     
-    func set_amount(_ mol: Double) -> Bool {
-        if mol > 0 {
-            self.mol = mol
-            return true
-        }
-        
-        return false
-    }
-    
-    // MARK: set concentrations in mol/L or mass fraction
-    
-    enum concentration: String {
-        case molL
-        case mass
-    }
-    
-    func set_concentration(_ kind: String, number: Double) -> Bool {
-        if let to_set = concentration(rawValue: kind) {
-            if number > 0 {
-                switch to_set {
-                case .molL:
-                    molL = number
-                case .mass:
-                    massfraction = number
-                }
-                return true
-            }
-        }
-        return false
-    }
-    
-    // MARK: Methods to update paramters
-    
-    enum willUpdate: String {
-        case mol
-        case mol_reverse
-        case weight
-        case weight_reverse
-        case molL
-        case mass
-    }
-    
-    func update(_ name: String) -> Bool {
-        if let to_update = willUpdate(rawValue: name) {
-            switch to_update {
-            case .mol:
-                if weight != nil && molecularweight != nil {
-                    mol = weight! / molecularweight!
-                    return true
-                }
-            case .mol_reverse:
-                if molL != nil && amount_h2o != nil {
-                    mol = molL! * (amount_h2o! / 1000.0)
-                    return true
-                }
-            case .weight:
-                if mol != nil && molecularweight != nil {
-                    weight = mol! * molecularweight!
-                    return true
-                }
-            case .weight_reverse:
-                if massfraction != nil && amount_h2o != nil {
-                    weight = amount_h2o! * massfraction! / (1 - massfraction!)
-                    return true
-                }
-            case .molL:
-                if mol != nil && amount_h2o != nil {
-                    molL = mol! / (amount_h2o! / 1000.0)
-                    return true
-                }
-            case .mass:
-                if weight != nil && amount_h2o != nil {
-                    massfraction = weight! / (weight! + amount_h2o!)
-                    return true
-                }
-            }
-        }
-        
-        return false
-    }
-
 }
