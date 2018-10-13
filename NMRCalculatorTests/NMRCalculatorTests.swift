457d752b675609a12d96290378a74440b45883bc
Jae-Seung Lee <jaeseung@gmail.com>
2018-10-13 16:32:36 -0400

    test: Add a test for calculate_dB()

--- NMRCalculatorTests/NMRCalculatorTests.swift
+++ NMRCalculatorTests/NMRCalculatorTests.swift
@@ -11,19 +11,40 @@ import XCTest
 
 class NMRCalculatorTests: XCTestCase {
     
+    var nmrCalcUnderTest: NMRCalc!
+    var nucleus: NMRNucleus!
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        nucleus = NMRNucleus()
+        nmrCalcUnderTest = NMRCalc(nucleus: nucleus)
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        nmrCalcUnderTest = nil
+        nucleus = nil
         super.tearDown()
     }
     
-    func testExample() {
+    func testCalculateDB() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let amp1 = 10000.0
+        let amp2 = 100.0
+        
+        let pulse1 = NMRPulse()
+        let pulse2 = NMRPulse()
+        
+        nmrCalcUnderTest.pulseNMR.append(pulse1)
+        nmrCalcUnderTest.pulseNMR.append(pulse2)
+        
+        let _ = nmrCalcUnderTest.setParameter("amplitude", in: "pulse1", to: amp1)
+        let _ = nmrCalcUnderTest.setParameter("amplitude", in: "pulse2", to: amp2)
+        let _ = nmrCalcUnderTest.calculate_dB()
+        
+        XCTAssertEqual(nmrCalcUnderTest.relativepower, -40.0)
     }
     
     func testPerformanceExample() {
