c7185c9e5da797673f83f0cdf80beeb95be2e1db
Jae-Seung Lee <jaeseung@gmail.com>
2019-11-23 14:41:00 -0500

    feat: Update SolutionTableViewCell.swift
    Add SolutionTableViewCellDelegate

--- NMRCalculator/View/SolutionTableViewCell.swift
+++ NMRCalculator/View/SolutionTableViewCell.swift
@@ -8,19 +8,52 @@
 
 import UIKit
 
+protocol SolutionTableViewCellDelegate: AnyObject {
+    func didEndEditing(_ cell: SolutionTableViewCell, for cellLabel: UILabel, newValue: Double?, error: String?)
+}
+
 class SolutionTableViewCell: UITableViewCell {
     @IBOutlet weak var itemLabel: UILabel!
     @IBOutlet weak var itemValue: UITextField!
     
+    // Variables
+    var sectionLabel: String?
+    var textBeforeEditing: String?
+    
+    weak var delegate: SolutionTableViewCellDelegate?
+    
     // MARK:- Methods
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        itemValue.delegate = self
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        // Configure the view for the selected state
     }
+}
 
+extension SolutionTableViewCell: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        //activeField = textField
+        textBeforeEditing = textField.text
+        textField.text = nil
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // activeField = nil
+        
+        guard let value = Double(textField.text!) else {
+            textField.text = textBeforeEditing
+            delegate?.didEndEditing(self, for: itemLabel, newValue: nil, error: "The input was not a number.")
+            return
+        }
+        
+        delegate?.didEndEditing(self, for: itemLabel, newValue: value, error: nil)
+    }
 }
