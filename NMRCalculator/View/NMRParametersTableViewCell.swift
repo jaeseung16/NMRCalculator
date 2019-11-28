c4871d6b6787e4bce898a167ff8c0a03958bbbfd
Jae-Seung Lee <jaeseung@gmail.com>
2019-11-28 10:13:53 -0500

    feat: Update NMRParametersTableViewCell.swift
    Add NMRParametersTableViewCellDelegate

--- NMRCalculator/View/NMRParametersTableViewCell.swift
+++ NMRCalculator/View/NMRParametersTableViewCell.swift
@@ -8,19 +8,68 @@
 
 import UIKit
 
+protocol NMRParametersTableViewCellDelegate: AnyObject {
+    func shouldBeginEditing(_ textField: UITextField) -> Bool
+    func shouldReturn()
+    func didBeginEditing(_ textField: UITextField)
+    func didEndEditing(_ cell: NMRParametersTableViewCell, for cellLabel: UILabel, newValue: Double?, error: String?)
+}
+
 class NMRParametersTableViewCell: UITableViewCell {
     // Properties
     @IBOutlet weak var itemLabel: UILabel!
     @IBOutlet weak var itemValue: UITextField!
     
+    // Variables
+    var sectionLabel: String?
+    var textBeforeEditing: String?
+    
+    weak var delegate: NMRParametersTableViewCellDelegate?
+    
     // MARK:- Methods
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        itemValue.delegate = self
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        // Configure the view for the selected state
+    }
+    
+    func setLabelAndValue(labelText: String?, valueText: String?) {
+        itemLabel.text = labelText
+        itemValue.text = valueText
+    }
+}
+
+extension NMRParametersTableViewCell: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return delegate?.shouldBeginEditing(textField) ?? false
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        delegate?.shouldReturn()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        //activeField = textField
+        textBeforeEditing = textField.text
+        textField.text = nil
+        delegate?.didBeginEditing(textField)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // activeField = nil
+        
+        guard let value = Double(textField.text!) else {
+            textField.text = textBeforeEditing
+            delegate?.didEndEditing(self, for: itemLabel, newValue: nil, error: "The input was not a number.")
+            return
+        }
+        
+        delegate?.didEndEditing(self, for: itemLabel, newValue: value, error: nil)
+        
     }
 }
