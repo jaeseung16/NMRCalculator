6beeb64d6df71bd2ad21e53a5ac4a00ffe6f55e2
Jae-Seung Lee <jaeseung@gmail.com>
2020-01-07 21:05:46 -0500

    test: Update NMRfidTests.swift
    Add tests: testSetSize(), testSetDuration(), testSetDwell(), testUpdateSize(), testUpdateDuration(), testUpdateDwell(), setDuration(), setDwell(), setSize()

--- NMRCalcTests/NMRfidTests.swift
+++ NMRCalcTests/NMRfidTests.swift
@@ -19,12 +19,12 @@ class NMRfidTests: XCTestCase {
     }
 
     override func tearDown() {
-              // Put teardown code here. This method is called after the invocation of each test method in the class.
-              nmrFid = nil
-              super.tearDown()
-          }
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        nmrFid = nil
+        super.tearDown()
+    }
 
-    func testNMRfidSet() {
+    func testSetSize() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         let parameter = "size"
@@ -33,6 +33,91 @@ class NMRfidTests: XCTestCase {
         let succeed = nmrFid.set(parameter: parameter, to: value)
         
         XCTAssertEqual(succeed, false, "size should be larger than 0")
+    }
+    
+    func testSetDuration() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let parameter = "duration"
+        let value = 0.0
+        
+        let succeed = nmrFid.set(parameter: parameter, to: value)
+        
+        XCTAssertEqual(succeed, false, "duration should be larger than 0")
+    }
+    
+    func testSetDwell() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let parameter = "dwell"
+        let value = 0.0
+        
+        let succeed = nmrFid.set(parameter: parameter, to: value)
+        
+        XCTAssertEqual(succeed, false, "dwell should be larger than 0")
+    }
+    
+    func testUpdateSize() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let oldSize = nmrFid.size
+        
+        setDuration()
+        setDwell()
+        
+        let sizeParamter = "size"
+        nmrFid.update(parameter: sizeParamter)
+        let newSize = nmrFid.size
+        
+        XCTAssertEqual(oldSize, newSize)
+    }
+    
+    func testUpdateDuration() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let oldDuration = nmrFid.duration
+        
+        setDwell()
+        setSize()
+        
+        let durationParamter = "duration"
+        nmrFid.update(parameter: durationParamter)
+        let newDuration = nmrFid.duration
+        
+        XCTAssertEqual(4.0 * oldDuration, newDuration)
+    }
+    
+    func testUpdateDwell() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let oldDwell = nmrFid.dwell
+        
+        setSize()
+        setDuration()
+    
+        let dwellParamter = "dwell"
+        nmrFid.update(parameter: dwellParamter)
+        let newDwell = nmrFid.dwell
+        
+        XCTAssertEqual(oldDwell, newDwell)
+    }
+    
+    private func setDuration() {
+        let durationParameter = "duration"
+        let newDuration = 2.0 * nmrFid.duration
+        nmrFid.set(parameter: durationParameter, to: newDuration)
+    }
+    
+    private func setDwell() {
+        let dwellParameter = "dwell"
+        let newDwell = 2.0 * nmrFid.dwell
+        nmrFid.set(parameter: dwellParameter, to: newDwell)
+    }
+    
+    private func setSize() {
+        let sizeParameter = "size"
+        let newSize = 2.0 * Double(nmrFid.size)
+        nmrFid.set(parameter: sizeParameter, to: newSize)
     }
 
 }
