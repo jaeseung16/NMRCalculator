9f7e36c61eaed645abb9b70495ae6fb7c6f84ecd
Jae-Seung Lee <jaeseung@gmail.com>
2020-01-12 12:20:17 -0500

    feat: WatchNMRCalculator
    Initial commit

---
+++ WatchNMRCalculator Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WatchNMRCalculator Extension
+//
+//  Created by Jae Seung Lee on 1/12/20.
+//  Copyright © 2020 Jae-Seung Lee. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ WatchNMRCalculator Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  WatchNMRCalculator Extension
+//
+//  Created by Jae Seung Lee on 1/12/20.
+//  Copyright © 2020 Jae-Seung Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
