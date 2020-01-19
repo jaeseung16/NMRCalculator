2ccd99fa3f844486194fcca310072765af4c227e
Jae-Seung Lee <jaeseung@gmail.com>
2020-01-19 17:27:11 -0500

    feat: Add WatchNucleusTable.swift

---
+++ WatchNMRCalculator Extension/WatchNucleusTable.swift
@@ -0,0 +1,12 @@
+//
+//  WatchNucleusTable.swift
+//  WatchNMRCalculator Extension
+//
+//  Created by Jae Seung Lee on 1/12/20.
+//  Copyright © 2020 Jae-Seung Lee. All rights reserved.
+//
+
+import SwiftUI
+
+let periodicTable = NMRPeriodicTable.shared
+
