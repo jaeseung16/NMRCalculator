d78a3824488d90fae5fce70e7fab50ec271fc659
Jae-Seung Lee <jaeseung@gmail.com>
2020-02-02 16:55:15 -0500

    refactor: Update WatchNucleusDetailView.swift
    Use Focus enum to determine which of LarmorFrequencyView and ProtonFrequencyView is selected

--- WatchNMRCalculator Extension/WatchNucleusDetailView.swift
+++ WatchNMRCalculator Extension/WatchNucleusDetailView.swift
@@ -10,17 +10,22 @@ import SwiftUI
 
 struct WatchNucleusDetailView: View {
     @EnvironmentObject var userData: UserData
-    @State private var changingProtonFrequency = true
+    @State var scrollAmountToFieldFactor: Double = 1
     
     var nucleus: NMRNucleus
     
     private var externalField: Double
     {
-        return self.userData.scrollAmount * 10.0
+        return self.userData.focus == .ExternalField ? self.userData.scrollAmount  : self.userData.scrollAmount / UserData.γProton
     }
     private var protonFrequency: Double
     {
-        return self.userData.scrollAmount * 10.0 * UserData().nuclei[0].γ
+        return self.userData.focus == .ProtonFrequency ? self.userData.scrollAmount : self.userData.scrollAmount * UserData.γProton
+    }
+    
+    private let minValue = 0.0
+    private var maxValue: Double {
+        return self.userData.focus == .ExternalField ? 100 : 2000
     }
     
     var body: some View {
@@ -49,25 +54,45 @@ struct WatchNucleusDetailView: View {
                 
                 LarmorFrequencyView(externalField: self.externalField,
                                     gyromatneticRatio: self.nucleus.γ)
-                    .foregroundColor(self.changingProtonFrequency ? .secondary : .primary)
+                    .foregroundColor(self.userData.focus == .ProtonFrequency ? .secondary : .primary)
                     .focusable(true) { _ in
-                        self.changingProtonFrequency = false
+                        if (self.userData.focus == .ProtonFrequency) {
+                            self.userData.focus = .ExternalField
+                            self.userData.scrollAmount /= UserData.γProton
+                        }
+//                        self.switchFocus()
                     }
-                .digitalCrownRotation(self.$userData.scrollAmount, from: 0.0, through: 10.0, by: 0.00001, sensitivity: .low, isContinuous: false, isHapticFeedbackEnabled: false)
+                .digitalCrownRotation(self.$userData.scrollAmount, from: self.minValue, through: self.maxValue, by: 0.01, sensitivity: .low, isContinuous: false, isHapticFeedbackEnabled: false)
                     .frame(width: geometry.size.width, height: geometry.size.height * 0.33)
                 
                 ProtonFrequencyView(protonFrquency: self.protonFrequency)
-                    .foregroundColor(self.changingProtonFrequency ? .primary : .secondary)
+                    .foregroundColor(self.userData.focus == .ProtonFrequency ? .primary : .secondary)
                     .focusable(true) { _ in
-                        self.changingProtonFrequency = true
+                        if (self.userData.focus == .ExternalField) {
+                            self.userData.focus = .ProtonFrequency
+                            self.userData.scrollAmount *= UserData.γProton
+                        }
+                        //self.switchFocus()
                     }
-                    .digitalCrownRotation(self.$userData.scrollAmount, from: 0.0, through: 10.0, by: 0.0000001, sensitivity: .low, isContinuous: false, isHapticFeedbackEnabled: false)
+                    .digitalCrownRotation(self.$userData.scrollAmount, from: self.minValue, through: self.maxValue, by: 0.01, sensitivity: .low, isContinuous: false, isHapticFeedbackEnabled: false)
                     .frame(width: geometry.size.width, height: geometry.size.height * 0.33)
             }
             .frame(width: geometry.size.width, height: geometry.size.height)
             .background(Color.black)
         }
     }
+    
+    // Why this is not working?
+//    private func switchFocus() {
+//        switch self.userData.focus {
+//        case .ExternalField:
+//            self.userData.focus = .ProtonFrequency
+//            self.userData.scrollAmount *= UserData.γProton
+//        case .ProtonFrequency:
+//            self.userData.focus = .ExternalField
+//            self.userData.scrollAmount /= UserData.γProton
+//        }
+//    }
 }
 
 struct WatchNucleusDetailView_Previews: PreviewProvider {
