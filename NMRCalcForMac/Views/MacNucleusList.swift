09797b1c80c6d81a26e97bc528cbdd29ae3d5e60
Jae-Seung Lee <jaeseung@gmail.com>
2021-02-22 19:10:06 -0500

    refactor: Clean up MacNucleusList.swift

--- NMRCalcForMac/Views/MacNucleusList.swift
+++ NMRCalcForMac/Views/MacNucleusList.swift
@@ -11,55 +11,40 @@ import SwiftUI
 struct MacNucleusList: View {
     @EnvironmentObject var userData: NMRPeriodicTableData
     @ObservedObject var calculator = MacNMRCalculator()
-    
-    @State var externalField = 1.0
-    @State private var isEditing = false
-    
+
     let proton = NMRNucleus()
     
     var nucleus: NMRNucleus {
         return calculator.nucleus ?? proton
     }
     
-    var elementSymbol: String {
-        return nucleus.symbolNucleus
-    }
-    
-    var atomicWeight: UInt {
-        return UInt(nucleus.atomicWeight)!
-    }
-    
-    var gyromagneticRatio: Double {
-        return Double(nucleus.gyromagneticRatio)!
-    }
-    
     var body: some View {
         HStack(alignment: .center) {
             MacLamorFrequencyView()
-            .environmentObject(calculator)
-            .frame(width: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .environmentObject(calculator)
+                .frame(width: 300, alignment: .center)
             
             Divider()
             
-            List(selection: $calculator.nucleus, content: {
+            List(selection: $calculator.nucleus) {
                 ForEach(userData.nuclei, id: \.self) { nucleus in
                     HStack {
                         Spacer()
                         MacNucleusView(nucleus: nucleus)
-                            .frame(width: 150, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .frame(width: 150, height: 100, alignment: .center)
                             .tag(nucleus)
                         Spacer()
                     }
                 }
-            })
+            }
             .listStyle(PlainListStyle())
-            .onChange(of: calculator.nucleus, perform: { value in
+            .onChange(of: calculator.nucleus) { value in
                 _ = calculator.$nucleus.sink { nucleus in
                     calculator.nucluesUpdated()
                 }
-            })
+            }
         }
-        .frame(width: 500, height: 400, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        .frame(width: 500, height: 400, alignment: .center)
     }
 }
 
