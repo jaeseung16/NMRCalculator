94d8197d372a5391d9974caea09b16a6e4969ca7
Jae-Seung Lee <jaeseung@gmail.com>
2023-09-16 17:16:26 -0400

    Refactor: Tap gesture to change scroll amount

--- WatchNMRCalculator2 Watch App/Views/WatchNucleusDetailView.swift
+++ WatchNMRCalculator2 Watch App/Views/WatchNucleusDetailView.swift
@@ -11,9 +11,6 @@ import SwiftUI
 struct WatchNucleusDetailView: View {
     @EnvironmentObject var userData: NMRPeriodicTableData
     
-    @FocusState private var focusedLarmorFrequency: Bool
-    @FocusState private var focusedProtonFrequency: Bool
-    @FocusState private var focus: NMRPeriodicTableData.Focus?
     @State var scrollAmountToFieldFactor: Double = 1
     
     var nucleus: NMRNucleus
@@ -53,17 +50,6 @@ struct WatchNucleusDetailView: View {
                 .foregroundColor(.green)
                 .frame(width: geometry.size.width, height: geometry.size.height * 0.33)
                 
-                //LarmorFrequencyView(externalField: externalField, gyromatneticRatio: nucleus.γ)
-                    //.foregroundColor(focus == .ProtonFrequency ? .secondary : .primary)
-                    //.focusable(true) { _ in
-                    //    if (userData.focus == .ProtonFrequency) {
-                    //        userData.focus = .ExternalField
-                    //        userData.scrollAmount /= NMRPeriodicTableData.γProton
-                    //    }
-                    //}
-                    //.frame(width: geometry.size.width, height: geometry.size.height * 0.33)
-                    //.focused($focus, equals: .ExternalField)
-                    //.focused($focusedLarmorFrequency)
                 VStack(alignment: .trailing, spacing: 0) {
                     HStack(alignment: .top) {
                         Text(String(format: "%.4f", externalField * nucleus.γ))
@@ -83,24 +69,9 @@ struct WatchNucleusDetailView: View {
                 }
                 .foregroundColor(userData.focus == .ProtonFrequency ? .secondary : .primary)
                 .frame(width: geometry.size.width, height: geometry.size.height * 0.33)
-                .focusable(true) { _ in
-                    if (userData.focus == .ProtonFrequency) {
-                        userData.focus = .ExternalField
-                        userData.scrollAmount /= NMRPeriodicTableData.γProton
-                    }
+                .onTapGesture {
+                    change(focus: .ExternalField)
                 }
-                
-                //ProtonFrequencyView(protonFrquency: protonFrequency)
-                    //.foregroundColor(focus == .ProtonFrequency ? .primary : .secondary)
-                    //.focusable(true) { _ in
-                    //    if (userData.focus == .ExternalField) {
-                    //        userData.focus = .ProtonFrequency
-                    //        userData.scrollAmount *= NMRPeriodicTableData.γProton
-                    //   }
-                    //}
-                    //.frame(width: geometry.size.width, height: geometry.size.height * 0.33)
-                    //.focused($focus, equals: .ProtonFrequency)
-                    //.focused($focusedProtonFrequency)
                 
                 VStack(alignment: .center, spacing: 0) {
                     Text("Proton")
@@ -115,54 +86,24 @@ struct WatchNucleusDetailView: View {
                 }
                 .foregroundColor(userData.focus == .ProtonFrequency ? .primary : .secondary)
                 .frame(width: geometry.size.width, height: geometry.size.height * 0.33)
-                .focusable(true) { _ in
-                    if (userData.focus == .ExternalField) {
-                        userData.focus = .ProtonFrequency
-                        userData.scrollAmount *= NMRPeriodicTableData.γProton
-                   }
+                .onTapGesture {
+                    change(focus: .ProtonFrequency)
                 }
             }
+            .focusable()
             .digitalCrownRotation($userData.scrollAmount, from: minValue, through: maxValue, by: 0.01, sensitivity: .low, isContinuous: false, isHapticFeedbackEnabled: false)
             .frame(width: geometry.size.width, height: geometry.size.height)
             .background(Color.black)
         }
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                focus = .ExternalField
-            }
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                focus = .ProtonFrequency
-            }
-        }
-        .onChange(of: focusedLarmorFrequency) { _ in
-            print("focusedLarmorFrequency=\(focusedLarmorFrequency)")
-            if focusedLarmorFrequency {
-                userData.focus = .ExternalField
-                userData.scrollAmount /= NMRPeriodicTableData.γProton
-            }
-        }
-        .onChange(of: focusedLarmorFrequency) { _ in
-            print("focusedLarmorFrequency=\(focusedLarmorFrequency)")
-            if focusedProtonFrequency {
-                userData.focus = .ProtonFrequency
-                userData.scrollAmount *= NMRPeriodicTableData.γProton
-            }
-        }
-        .onChange(of: focus) { _ in
-            print("focus=\(String(describing: focus))")
-            switch focus {
-            case .ExternalField:
-                userData.focus = .ExternalField
-                userData.scrollAmount /= NMRPeriodicTableData.γProton
-            case .ProtonFrequency:
-                userData.focus = .ProtonFrequency
-                userData.scrollAmount *= NMRPeriodicTableData.γProton
-            default:
-                userData.focus = .ExternalField
-                userData.scrollAmount /= NMRPeriodicTableData.γProton
-            }
-            
+    }
+    
+    private func change(focus: NMRPeriodicTableData.Focus) -> Void {
+        userData.focus = focus
+        switch focus {
+        case .ExternalField:
+            userData.scrollAmount /= NMRPeriodicTableData.γProton
+        case .ProtonFrequency:
+            userData.scrollAmount *= NMRPeriodicTableData.γProton
         }
     }
 
