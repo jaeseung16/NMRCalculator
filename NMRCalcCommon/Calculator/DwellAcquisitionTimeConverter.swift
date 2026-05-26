41e7bec8c1555f0e7b5dbc0467b19f2eba47f7de
Jae-Seung Lee <jaeseung@gmail.com>
2026-05-26 17:21:58 -0400

    refactor: Update DwellAcquisitionTimeConverter to use NMRCalcDelegate

--- NMRCalcCommon/Calculator/DwellAcquisitionTimeConverter.swift
+++ NMRCalcCommon/Calculator/DwellAcquisitionTimeConverter.swift
@@ -7,8 +7,12 @@
 //
 
 import Foundation
+import os
+import NMRCalculatorCommon
 
 class DwellAcquisitionTimeConverter {
+    private static let logger = Logger()
+    
     private static let secToμs: Double = 1000000.0
     private static var μsToSec: Double {
         get {
@@ -20,57 +24,77 @@ class DwellAcquisitionTimeConverter {
     public var numberOfPoints: Int //
     public var dwell: Double // sec
     
+    private let delegate: NMRCalcDelegate
+    private var error: NMRCalcError?
+    
     public var dwellInμs: Double {
-        return dwell * DwellAcquisitionTimeConverter.secToμs
+        return dwell * Self.secToμs
     }
     
-    public init(acqusitionTime: Double, dwell: Double) {
-        self.acqusitionTime = acqusitionTime
-        self.dwell = dwell
-        self.numberOfPoints = Int(acqusitionTime / dwell)
-    }
-    
-    public init(acqusitionTime: Double, numberOfPoints: Int) {
+    public init(acqusitionTime: Double, numberOfPoints: Int, dwell: Double) {
         self.acqusitionTime = acqusitionTime
         self.numberOfPoints = numberOfPoints
-        self.dwell = acqusitionTime / Double(numberOfPoints)
-    }
-    
-    public init(dwell: Double, numberOfPoints: Int) {
         self.dwell = dwell
-        self.numberOfPoints = numberOfPoints
-        self.acqusitionTime = Double(numberOfPoints) * dwell
+        self.delegate = NMRCalcFactory.shared.create(.time)
     }
     
-    private func updateNumberOfPoints() {
-        numberOfPoints = Int(acqusitionTime / dwell)
+    public convenience init(acqusitionTime: Double, dwell: Double) {
+        self.init(acqusitionTime: acqusitionTime,
+                  numberOfPoints: Int(acqusitionTime / dwell),
+                  dwell: dwell)
     }
     
-    private func updateDwell() {
-        dwell = acqusitionTime / Double(numberOfPoints)
+    public convenience init(acqusitionTime: Double, numberOfPoints: Int) {
+        self.init(acqusitionTime: acqusitionTime,
+                  numberOfPoints: numberOfPoints,
+                  dwell: acqusitionTime / Double(numberOfPoints))
     }
     
-    private func updateAcquisitionTime() {
-        acqusitionTime = Double(numberOfPoints) * dwell
+    public convenience init(dwell: Double, numberOfPoints: Int) {
+        self.init(acqusitionTime: Double(numberOfPoints) * dwell,
+                  numberOfPoints: numberOfPoints,
+                  dwell: dwell)
+    }
+    
+    private func update(from response: TimeDomainResponse) {
+        self.acqusitionTime = response.acqusitionTimeInSec
+        self.numberOfPoints = response.numberOfPoints
+        self.dwell = response.dwellInSec
+    }
+    
+    private func process(_ request: NMRCalcRequest) {
+        let result = delegate.process(request)
+        switch result {
+        case .success(let response):
+            guard let response = response as? TimeDomainResponse else {
+                Self.logger.error("Invalid response type: \(type(of: response))")
+                error = .invalidOutput
+                break
+            }
+            update(from: response)
+        case .failure(let error):
+            Self.logger.error("Failed to process: \(String(describing: request))")
+            self.error = error
+        }
     }
 
     public func set(acqusitionTime: Double) -> Void {
-        self.acqusitionTime = acqusitionTime
-        updateDwell()
+        let request = TimeDomainRequest(acqusitionTimeInSec: acqusitionTime, numberOfPoints: numberOfPoints)
+        process(request)
     }
     
     public func set(dwell: Double) -> Void {
-        self.dwell = dwell
-        updateAcquisitionTime()
+        let request = TimeDomainRequest(numberOfPoints: numberOfPoints, dwellInSec: dwell)
+        process(request)
     }
     
     public func set(dwellInμs: Double) -> Void {
-        self.dwell = dwellInμs * DwellAcquisitionTimeConverter.μsToSec
-        updateAcquisitionTime()
+        let request = TimeDomainRequest(numberOfPoints: numberOfPoints, dwellInSec: dwellInμs * Self.μsToSec)
+        process(request)
     }
     
     public func set(numberOfPoints: Int) -> Void {
-        self.numberOfPoints = numberOfPoints
-        updateDwell()
+        let request = TimeDomainRequest(acqusitionTimeInSec: acqusitionTime, numberOfPoints: numberOfPoints)
+        process(request)
     }
 }
